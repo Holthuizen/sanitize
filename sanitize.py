17e1697368d746805789b1a6a2d96fa5b76fd31f
Holthuizen <45522614+Holthuizen@users.noreply.github.com>
2020-10-24 16:32:47 +0200

    basic sanitation function

---
+++ sanitize.py
@@ -0,0 +1,36 @@
+def sanitize(inputstr):
+    sanitized = ''
+    bad_characters = [
+        ';',
+        '*',
+        '<',
+        '>',
+        '\'',
+        '1,2',
+        '`',
+        '(',
+        ')',
+    ]
+    bad_strings=[
+        'file://',
+        'input://',
+        '../',
+        '\x00',
+        '%3C',
+        '--',
+        '&&',
+        '\'',
+    ]
+    #remove all dangerous chars. 
+    for char in inputstr:  
+        sanitized += '' if (char in bad_characters) else char 
+    #now replace dangerous strings with a non dangerous char. (if you remove instead you get: f--le:// -> file://)
+    for s in bad_strings:
+        try:
+            sanitized = sanitized.replace(s,'#')
+        except:
+            pass
+    return sanitized.strip(' ')
+
+
+print(sanitize("  && hello %3C&'& world"))
